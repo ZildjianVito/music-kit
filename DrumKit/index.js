fdc6fcdab38e9519927e95d7b2373b8bb75448e2
ZildjianVito <109806543+ZildjianVito@users.noreply.github.com>
2022-10-09 18:22:46 +0700

    Update index.js

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -26,7 +26,7 @@ function makeSound(key) {
             tom1.play();
             break;
         case "a":
-            var tom2 = new Audio("Sounds/tom-2.mp3");
+            var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
         case "s":
